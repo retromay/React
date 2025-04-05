48bbb3560b4de838b7235157b0a65d4495959210
Mayank <mayanknath1997@gmail.com>
2025-04-05 16:45:17 +0530

    Feat: Reach revision

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+
+   
+  return(
+    <>
+    <Home/>
+    </>
+ 
+  )
+}
+
+
+
+export default App
---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+
+function Home(){
+    const [searchQuery, setSearchQuery]= useState("");
+
+    const movies =[
+        {id:1,title:"dog",release_date:"2020"},
+        {id:2,title:"dog2",release_date:"2021"},
+        {id:3,title:"dog3",release_date:"2022"},
+    ]
+
+    const handleSearch =(e) =>{
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("---------")
+    }
+
+    return(
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                type="text" 
+                placeholder="Search for movies...." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">
+                    search
+                </button>
+            </form>
+            <div className="movies-grid">
+                {movies.map( movie => 
+                    (<MovieCard movie={movie} key={movie.id}/>))}
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
