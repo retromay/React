9eae988cb12e5e86388a1271af72bd7d8f1be8c0
Mayank <mayanknath1997@gmail.com>
2025-04-09 09:49:53 +0530

    updated javascrpit interview

---
+++ jsinterview/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 3);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+
+
+console.log(undefined === 0);
+
+// >=,<= works differently than == and will convert null into number and treating 
+//it as 0.
