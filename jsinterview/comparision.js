476eed99075748e433e4b90f3e22c46005ed56df
Mayank <mayanknath1997@gmail.com>
2025-04-12 21:03:04 +0530

    JavaScroit update

--- jsinterview/comparision.js
+++ jsinterview/comparision.js
@@ -20,7 +20,23 @@
 // === 
 
 
-console.log(undefined === 0);
+// console.log(undefined === 0);
 
 // >=,<= works differently than == and will convert null into number and treating 
 //it as 0.
+let count = 0
+function fun() {
+    
+    return () => {
+        let y = count;
+        console.log(++y);
+    };
+}
+console.log(count)
+const x = fun();
+console.log(count)
+x();
+console.log(count)
+x();
+
+x();
